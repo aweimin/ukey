7493dddb72c57fa8758ed4113a4c45a91d6440a7
sunshine <wz_88@live.cn>
2026-01-19 16:39:17 +0800

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // 导出所有UKey相关模块
 export { IUKeyWebSocketClient } from './IUKeyWebSocketClient';
 export { UKeyWebSocketClient } from './UKeyWebSocketClient';
+export { UKeyClient } from './UKeyClient';
 export { UKeyUtils } from './UKeyUtils';
 export { UniqueIdGenerator } from './UniqueIdGenerator';
 export { UKeyResponse } from './types';
